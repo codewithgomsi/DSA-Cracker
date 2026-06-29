194567501a15604f02dfe907cc13bb08aac9ef0b
codewithgomsi <prathameshwork54321@gmail.com>
2026-06-29 18:50:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove Two Consecutive Same/remove-two-consecutive-same.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int removeConsecutiveSame(vector<string>& arr) {
+        // code here
+        stack<string>s;
+        for(int i=0;i<arr.size();i++){
+            if(s.empty()){
+                s.push(arr[i]);
+            }else{
+                if(s.top()==arr[i]){
+                    s.pop();
+                }else{
+                    s.push(arr[i]);
+                }
+            }
+        }
+        return s.size();
+    }
+};
