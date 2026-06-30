b0f2aad9d687a38697da49ba0ac69e94b60f43d9
codewithgomsi <prathameshwork54321@gmail.com>
2026-06-30 10:42:53 +0530

    Time: 0 ms (100%), Space: 8.8 MB (89.3%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,5 +1,23 @@
 class Solution {
 public:
+// army logic -bas thoda change hai- my  logic more beter
+bool isValid(string s) {
+        stack<char>st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('||s[i]=='['||s[i]=='{'){
+                st.push(s[i]);
+            }else if(st.empty()){
+                return 0;;
+            }else{
+                if(st.top()=='('&&s[i]==')'||st.top()=='{'&&s[i]=='}'||st.top()=='['&&s[i]==']'){
+                    st.pop();
+                }else{
+                    return 0;
+                }
+            }
+        }
+        return st.size()==0;
+/*
     bool isValid(string s) {
         stack<char>st;
         for(int i=0;i<s.size();i++){
@@ -11,6 +29,6 @@ public:
                 st.push(s[i]);
             }
         }
-        return st.size()==0;
+        return st.size()==0;*/
     }
 };
