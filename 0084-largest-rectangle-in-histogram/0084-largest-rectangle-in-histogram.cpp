9dbe042e135747124d633591f7c9b495c5bd6b48
codewithgomsi <prathameshwork54321@gmail.com>
2026-07-12 19:26:41 +0530

    Time: 20 ms (64.89%), Space: 89.4 MB (10.7%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
+        
+        /*
+        int ans=0;
+        stack<int>st;
+        for(int i=0;i<nsr.size();i++){
+             // we have found the next smaller
+             // i is nsr
+            while(!st.empty()&&heights[st.top()]>heights[i]){
+                int height_index=st.top();
+                st.pop();
+                ans=max(ans,heights[height_index]*(i-st.top()-1));
+            }
+            st.push(i);
+        }
+        // kuch element stack ma reh jainga
+        // yeah woh element honga jo ka end horaha honga at nth position
+        while(!st.empty()){
+           int height_index=st.top();
+           st.pop();
+           if(!st.empty()){
+                ans=max(ans,heights[height_index]*(heights.size()-st.top()-1));   }
+                else{
+                ans=max(ans,heights[height_index]*heights.size());
+                }
+        }  */
+        // first approach
         // NEXT SMALLEST RIGHT
         // NEXT SMALLEST LEFT
         // abv logic to find the possible rectangle for each element 
@@ -8,17 +34,18 @@ public:
         vector<int>nsl(heights.size());
         int ans=0;
         stack<int>st;
+        // finding next smaller element on right
         for(int i=0;i<nsr.size();i++){
             while(!st.empty()&&heights[st.top()]>heights[i]){
                 nsr[st.top()]=i;
                 st.pop();
             }
             st.push(i);
-        }
+        }// empty the stack
         while(!st.empty()){
             nsr[st.top()]=heights.size();
                 st.pop();
-        }  
+        }  // finding next smaller element on left
         for(int i=nsl.size()-1;i>=0;i--){
             while(!st.empty()&&heights[st.top()]>heights[i]){
                 nsl[st.top()]=i;
@@ -26,14 +53,17 @@ public:
             }
             st.push(i);
         }
+        // empty the stack
         while(!st.empty()){
             nsl[st.top()]=-1;
                 st.pop();
         }
+        // finding largest rectangle
        for(int i=0;i<heights.size();i++){
         ans=max(ans,(heights[i]*(nsr[i]-nsl[i]-1)));
        }
        return ans;
     }
-    // little m
+   
+
 };
